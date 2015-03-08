7f813a6b1374c3e4c70df4ce7319001f2c7e38f2
thibauts <thibaut.seguy@gmail.com>
2015-03-08 11:56:52 +0100

    removed the module compat cruft and bumped to 1.0.0

--- index.js
+++ index.js
@@ -1,94 +1,59 @@
-(function() {
+var numeric = require('numeric');
 
-  // Node
-  if(typeof module !== 'undefined' && module.exports) {
-    var numeric = require('numeric');
-    onload(numeric);
+function distanceLinear(pa, pb) {
+  return numeric.norm2(numeric.sub(pb, pa));
+}
+
+function distanceCubic(pa, pb) {
+  return Math.pow(distanceLinear(pa, pb), 3);
+}
+
+var distanceFunctions = {
+  'linear': distanceLinear,
+  'cubic':  distanceCubic
+};
+
+function RBF(points, values, distanceFunction) {
+
+  var distance = distanceFunctions.linear;
+
+  if(distanceFunction) {
+    distance = typeof distanceFunction !== 'string'
+      ? distanceFunction
+      : distanceFunctions[distanceFunction];
   }
-  // AMD
-  else if(typeof define !== 'undefined' && define.amd) {
-    define(['numeric'], onload);
+
+  // `identity` here serves as a shorthand to allocate
+  // the matrix nested array.
+  var M = numeric.identity(points.length);
+
+  for(var j=0; j<points.length; j++) {
+    for(var i=0; i<points.length; i++) {
+      M[j][i] = distance(points[i], points[j]);
+    }
   }
-  // Browser
-  else {
-    onload(window.numeric);
+
+  // Compute basis functions weights by solving
+  // the linear system of equations
+  var LU = numeric.LU(M);
+  var w = numeric.LUsolve(LU, values);
+
+  // The returned interpolant will compute the value at any point 
+  // by summing the weighted contributions of the input points
+  function interpolant(p) {
+    var distances = points.map(function(point) {
+      return distance(p, point);
+    });
+
+    var products = numeric.mul(distances, w);
+    var sum = products.reduce(function(acc, value) {
+      return acc + value;
+    }, 0);
+
+    return sum;
   }
 
-  function onload(numeric) {
+  return interpolant;
+}
 
-    function distanceLinear(pa, pb) {
-      return numeric.norm2(numeric.sub(pb, pa));
-    }
-
-    function distanceCubic(pa, pb) {
-      return Math.pow(distanceLinear(pa, pb), 3);
-    }
-
-    var distanceFunctions = {
-      'linear': distanceLinear,
-      'cubic':  distanceCubic
-    };
-
-    function RBF(points, values, distanceFunction) {
-
-      var distance = distanceFunctions.linear;
-
-      if(distanceFunction) {
-        distance = typeof distanceFunction !== 'string'
-          ? distanceFunction
-          : distanceFunctions[distanceFunction];
-      }
-
-      // `identity` here serves as a shorthand to allocate
-      // the matrix nested array.
-      var M = numeric.identity(points.length);
-
-      for(var j=0; j<points.length; j++) {
-        for(var i=0; i<points.length; i++) {
-          M[j][i] = distance(points[i], points[j]);
-        }
-      }
-
-      // Compute basis functions weights by solving
-      // the linear system of equations
-/*      
-      var invM = numeric.inv(M);
-      var w = numeric.dot(invM, values);
-*/
-      var LU = numeric.LU(M);
-      var w = numeric.LUsolve(LU, values);
-
-      // The returned interpolant will compute the value at any point 
-      // by summing the weighted contributions of the input points
-      function interpolant(p) {
-        var distances = points.map(function(point) {
-          return distance(p, point);
-        });
-
-        var products = numeric.mul(distances, w);
-        var sum = products.reduce(function(acc, value) {
-          return acc + value;
-        }, 0);
-
-        return sum;
-      }
-
-      return interpolant;
-    }
-
-    // Node
-    if(typeof module !== 'undefined' && module.exports) {
-      module.exports = RBF;
-    }
-    // AMD
-    else if(typeof define !== 'undefined' && define.amd) {
-      return RBF;
-    }
-    // Browser
-    else {
-      window.RBF = RBF;
-    }  
-
-  } // onload
-
-})();
+module.exports = RBF;
